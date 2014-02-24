f1e0df4cba72255b67299fd6d07a8e1f44647218
Adam England <adam.n.england@gmail.com>
2014-02-24 15:52:37 -0600

    Adding build number bump to the build scripts

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 rm knoda-release.apk
-./gradlew clean assembleRelease
+./gradlew incrementVersionCode clean assembleRelease
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore knoda-release.keystore Knoda/build/apk/Knoda-release-unsigned.apk Knoda
 jarsigner -verify Knoda/build/apk/Knoda-release-unsigned.apk
 /Applications/Android\ Studio.app/sdk/tools/zipalign -v 4 Knoda/build/apk/Knoda-release-unsigned.apk knoda-release.apk
