3c540918442ab8ec56bae0c0021f0976c9016e7e
nick <nrellihan@gmail.com>
2014-03-03 05:53:31 -0600

    fixes #73

--- Knoda/src/main/java/helpers/DateUtil.java
+++ Knoda/src/main/java/helpers/DateUtil.java
@@ -23,7 +23,7 @@ public class DateUtil {
         } else if (period.getMonths() != 0)
             return period.getMonths() + "mo";
         else if (period.getDays() != 0 || period.getWeeks() != 0) {
-            int days = period.getWeeks() != 0 ? period.getWeeks() * 7 : period.getDays();
+            int days = period.getWeeks() != 0 ? period.getWeeks() * 7 + period.getDays(): period.getDays();
             return days + "d";
         }
         else if (period.getHours() != 0)
