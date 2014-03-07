563822924e662589c270ee39dbdd99bdfa0844bf
Adam England <adam.n.england@gmail.com>
2014-03-06 18:22:12 -0600

    Added safegaurd to prevent accidental test builds to prod

--- release.sh
+++ release.sh
@@ -1,5 +1,11 @@
 rm knoda-release.apk
-./gradlew incrementVersionCode clean assembleRelease
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore knoda-release.keystore Knoda/build/apk/Knoda-release-unsigned.apk Knoda
-jarsigner -verify Knoda/build/apk/Knoda-release-unsigned.apk
-/Applications/Android\ Studio.app/sdk/tools/zipalign -v 4 Knoda/build/apk/Knoda-release-unsigned.apk knoda-release.apk
+./gradlew verifyRelease incrementVersionCode clean assembleRelease
+gradle_status=$?
+if [ $gradle_status == 0 ]; then
+    jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore knoda-release.keystore Knoda/build/apk/Knoda-release-unsigned.apk Knoda
+    jarsigner -verify Knoda/build/apk/Knoda-release-unsigned.apk
+    /Applications/Android\ Studio.app/sdk/tools/zipalign -v 4 Knoda/build/apk/Knoda-release-unsigned.apk knoda-release.apk
+else
+	echo "I refuse to build your release.  It appears that your configuration is pointing at test instead of prod.  Override me if you wish."
+fi
+
