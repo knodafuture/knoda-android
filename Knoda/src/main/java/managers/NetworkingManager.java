bf19f86d4bc9cbabc05e5e3315eae1b097534f22
nick <nrellihan@gmail.com>
2014-03-14 17:13:04 -0500

    well taht didnt work

--- Knoda/src/main/java/managers/NetworkingManager.java
+++ Knoda/src/main/java/managers/NetworkingManager.java
@@ -64,7 +64,7 @@ public class NetworkingManager {
     public static String termsOfServiceUrl = "http://knoda.com/terms";
     public static String privacyPolicyUrl = "http://knoda.com/privacy";
     public static Integer PAGE_LIMIT = 50;
-    public static String baseUrl = "http://api-test.knoda.com/api/";
+    public static String baseUrl = "http://api.knoda.com/api/";
 
     private ImageLoader imageLoader;
 
