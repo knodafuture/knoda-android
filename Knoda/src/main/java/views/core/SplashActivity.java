f48dffa8f25ab43a3f66c17d3e71135e0f876de6
nick <nrellihan@gmail.com>
2014-06-20 10:38:15 -0500

    alert on no internet

--- Knoda/src/main/java/views/core/SplashActivity.java
+++ Knoda/src/main/java/views/core/SplashActivity.java
@@ -1,10 +1,14 @@
 package views.core;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.RelativeLayout;
 
+import com.android.volley.NoConnectionError;
+import com.android.volley.VolleyError;
 import com.knoda.knoda.R;
 
 import butterknife.ButterKnife;
@@ -24,6 +28,45 @@ public class SplashActivity extends BaseActivity {
 
         setContentView(R.layout.activity_splash);
         ButterKnife.inject(this);
+
+    }
+
+
+    private boolean showNotConnectedToNetworkDialog(VolleyError error) {
+
+        if (error instanceof NoConnectionError) {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Your network connection appears to be down.  Please ensure that you are connected to a wireless network, and try again.")
+                    .setCancelable(false)
+                    .setNegativeButton("Try Again", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            Intent i = getBaseContext().getPackageManager()
+                                    .getLaunchIntentForPackage(getBaseContext().getPackageName());
+                            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivity(i);
+                            dialog.cancel();
+                        }
+                    })
+                    .setPositiveButton("Cancel", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    });
+
+            AlertDialog alert = builder.create();
+            alert.show();
+            return true;
+        }
+
+        return false;
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        com.facebook.AppEventsLogger.activateApp(getApplicationContext(), "455514421245892");
         userManager.loginSavedUser(new NetworkCallback<User>() {
             @Override
             public void completionHandler(User object, ServerError error) {
@@ -31,22 +74,24 @@ public class SplashActivity extends BaseActivity {
                 if (error == null) {
                     launchMainActivity();
                     return;
-                }
+                } else if (showNotConnectedToNetworkDialog(error.underlyingError))
+                    return;
 
                 userManager.loginAsGuest(new NetworkCallback<User>() {
                     @Override
                     public void completionHandler(User object, ServerError error) {
-                        launchMainActivity();
+
+                        if (error != null) {
+                            if (showNotConnectedToNetworkDialog(error.underlyingError))
+                                return;
+                            else
+                                errorReporter.showError(error);
+                        } else
+                            launchMainActivity();
                     }
                 });
             }
         });
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        com.facebook.AppEventsLogger.activateApp(getApplicationContext(), "455514421245892");
     }
 
     private void launchMainActivity() {
