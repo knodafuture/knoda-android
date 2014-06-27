c12a3de83881319f0b73d35b76aa152b3581ce83
Jeff Cailteux <jcailteux@knoda.com>
2014-06-27 10:38:01 -0500

    build scripts updated

--- release-test.sh
+++ release-test.sh
@@ -2,4 +2,4 @@ rm knoda-release.apk
 ./gradlew clean assembleRelease
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore knoda-release.keystore Knoda/build/outputs/apk/Knoda-release-unsigned.apk Knoda
 jarsigner -verify Knoda/build/outputs/apk/Knoda-release-unsigned.apk
-/Applications/Android\ Studio.app/sdk/tools/zipalign -v 4 Knoda/build/outputs/apk/Knoda-release-unsigned.apk knoda-release.apk
+/Applications/Android\ Studio.app/sdk/build-tools/19.1.0/zipalign -v 4 Knoda/build/outputs/apk/Knoda-release-unsigned.apk knoda-release.apk
