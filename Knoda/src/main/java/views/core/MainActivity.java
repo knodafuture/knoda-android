a8bd2e1f4ecc3a47c4f9a3395533144a228ad7bf
Jeff Cailteux <jcailteux@knoda.com>
2014-07-10 13:40:00 -0500

    logging in of a real user

--- Knoda/src/main/java/views/core/MainActivity.java
+++ Knoda/src/main/java/views/core/MainActivity.java
@@ -148,6 +148,8 @@ public class MainActivity extends BaseActivity {
                     spinner.hide();
                 }
             });
+        } else {
+            restart();
         }
     }
 
